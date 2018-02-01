ce8fca05b90fc4e2d1f58795c094c9f7a8e66a95
Thalia Villalobos <vill9422@m506-118-26441.local>
2018-01-31 21:02:51 -0800

    added movie image to imageview

--- Fix/MovieCell.swift
+++ Fix/MovieCell.swift
@@ -14,6 +14,8 @@ class MovieCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+
 
 
     override func awakeFromNib() {
--- Fix/NowPlayingViewController.swift
+++ Fix/NowPlayingViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import AlamofireImage
 
+
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
@@ -63,9 +64,14 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPathString = movie["poster_path"] as! String
+        let baseURLString = "https://image.tmdb.org/t/p/w500"
+        let posterURL  = URL(string: baseURLString + posterPathString)!
+        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.posterImageView.af_setImage(withURL: posterURL)
         
         return cell
     }
