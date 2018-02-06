47655b335b7bc2fc3778dee65ac5e9a7db31eea2
Thalia Villalobos <vill9422@m506-246-25987.local>
2018-02-05 19:45:09 -0800

    setup collection view

---
+++ Fix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Fix
+//
+//  Created by Mario Martinez on 2/1/18.
+//  Copyright © 2018 Thalia Villalobos. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
